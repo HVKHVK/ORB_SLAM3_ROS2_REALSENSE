d0385c7d84211cc71f811ebc50708fa2482e8b93
H. Volkan Kasikaralar <vkasikaralar@gmail.com>
2023-06-19 20:44:32 +0200

    Fixed Thirdparty

---
+++ build_ros2.sh
@@ -0,0 +1,55 @@
+echo "Configuring and building Thirdparty/Pangolin ..."
+
+cd Thirdparty/Pangolin
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4
+
+cd ../../DBoW2
+
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4
+
+cd ../../Sophus
+
+echo "Configuring and building Thirdparty/Sophus ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4
+
+cd ../../../
+
+echo "Uncompress vocabulary ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
+
+echo "Configuring and building ORB_SLAM3 ..."
+
+mkdir build
+cd buil
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+cd ..
+echo $PWD
+echo "Building ROS nodes"
+
+cd ROS2
+colcon build --parallel-workers 4
